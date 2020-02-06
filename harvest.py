6035a362e7fa0316ba91304053b75ffb3753aa7a
Your Name <you@example.com>
2020-02-06 14:18:57 -0800

    Class Melon is_sellable() tentatively implemented

--- harvest.py
+++ harvest.py
@@ -18,12 +18,10 @@ class MelonType(object):
 
         self.pairings = []
 
-
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
 
         self.pairings.append(pairing)
-
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
@@ -56,6 +54,7 @@ def make_melon_types():
     # import pdb; pdb.set_trace()
 
     return all_melon_types
+
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
@@ -94,21 +93,33 @@ def make_melon_type_lookup(melon_types):
 # Part 2   #
 ############
 
-class Melon(object):
+
+class Melon(MelonType):
     """A melon in a melon harvest."""
 
     # Fill in the rest
     # Needs __init__ and is_sellable methods
+    def __init__(self, shape_rate, color_rate, harvested_from, harvested_by):
+        """Initialize a melon."""
+        # inherits self.code from parent class MelonType
+        self.shape_rate = shape_rate
+        self.color_rate = color_rate
+        self.harvested_from = harvested_from
+        self.harvested_by = harvested_by
+
+    def is_sellable(melon):
+        """Checks whether a melon has rating higher than 5 and isn't from Field 3."""
+
+        return (melon.shape_rate and melon.color_rate > 5) and (melon.harvested_from != 'Field 3')
+
 
 def make_melons(melon_types):
     """Returns a list of Melon objects."""
 
     # Fill in the rest
 
+
 def get_sellability_report(melons):
     """Given a list of melon object, prints whether each one is sellable."""
 
-    # Fill in the rest 
-
-
-
+    # Fill in the rest
